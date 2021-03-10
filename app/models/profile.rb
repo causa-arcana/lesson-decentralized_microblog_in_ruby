9bd7a6fa13f0ee8f9c903e7a90c7770f88da4472
Alex Kotov <kotovalexarian@gmail.com>
2021-03-10 14:59:09 +0500

    Improve method Profile#full_name

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,7 @@ class Profile < ApplicationRecord
   has_many :posts
 
   def full_name
-    [first_name, last_name].map(&:presence).compact.join ' '
+    [first_name, last_name].map(&:presence).compact.join(' ').presence ||
+      "Profile#{" ##{id}" if id.present?}"
   end
 end
