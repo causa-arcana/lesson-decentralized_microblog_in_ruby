7c1ec6acf52627ac1eef2006f457f108c508ca43
Alex Kotov <kotovalexarian@gmail.com>
2021-03-10 15:41:07 +0500

    Add associations Profile#outgoing_followships, #incoming_followships

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,14 @@
 class Profile < ApplicationRecord
   has_many :posts
 
+  has_many :outgoing_followships,
+           class_name: 'Followship',
+           foreign_key: :subject_profile_id
+
+  has_many :incoming_followships,
+           class_name: 'Followship',
+           foreign_key: :object_profile_id
+
   def full_name
     [first_name, last_name].map(&:presence).compact.join(' ').presence ||
       "Profile#{" ##{id}" if id.present?}"
